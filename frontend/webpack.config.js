e927d5b4dd871c18b9d924a784984b6b22fac87f
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-04-21 00:14:25 -0700

    feat: source map loader

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,5 +4,15 @@ module.exports = {
     output: {
         filename: 'main.js',
         publicPath: 'dist/'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            }
+        ]
     }
 };
